7b8d67f977fd40aa6afe4bbe65004690e51a9a0b
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-19 17:35:17 -0600

    Update rules to add description and finalize home comp

---
+++ main.js
@@ -0,0 +1,25 @@
+//Data Model
+
+//some of these vars can be put directly into functions
+var userWins = 0;
+var computerWins = 0;
+var classicOptions = [];
+var benderOptions = [];
+var userFighter;
+var computerFighter;
+
+
+//Global Variables
+
+//Event Listeners
+
+//Functions
+
+/* 
+PROCESS FOR CLASSIC:
+
+PROCESS FOR BENDER:
+
+
+*/
+
