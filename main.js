96e2c31277338758365ccd10013cd4669c108202
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-24 17:38:41 -0600

    store info about fighters in array to use for loop to determine winner

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 //Data Model
-var currentGame;
-var user = createPlayer('user', '🤗')
-var computer = createPlayer('computer', '💻')
+var game = createGame('game')
+
+//Global Variable
+var fightersInfo = [
+    {
+        name: 'rock',
+        beats: ['scissors'],
+        imgLink: "https://lh3.googleusercontent.com/drive-viewer/AAOQEOSzmgDCFMwpoQmc3HpGtiZbBsLUbU8U_Q8ieUHwd5vULs5DPYnbllzG8gT4MocpyT_pYLpCqMKxKgHNlN2ShlzmddN5dA=s2560"
+    },
+    {
+        name: 'paper',
+        beats: ['rock'],
+        imgLink: "https://lh3.googleusercontent.com/drive-viewer/AAOQEORV8Y7IGEjBOcarUqfjlUeEGKIsfiKAcbZTfT7cs4arv4joLWXCmYpTJUwE9emUdkQpVGCVg33ytaWLChEk3CLGdom1Pw=s2560"
+    },
+    {
+        name: 'scissors',
+        beats: ['paper'],
+        imgLink: "https://lh3.googleusercontent.com/drive-viewer/AAOQEORxY6MonbE2QikFBOjEu9i118ZDTzviC-2O7CDugbTL28Q0LtXK75lLHQxYF_EBdckneOsvw84kvGS-Fk6oHJJ4iK77lA=s2560"
+    },
+    {
+        name: 'earth',
+        beats: ['air', 'fire'],
+        imgLink: "https://lh3.googleusercontent.com/drive-viewer/AAOQEOTUHJKlKO_XulXT4bZuvS1gIjLCV1gEQO4pPdpfuWK9zdjBRW3_YqG8A93DtIyC-lb3p9HkadHtld1ACkkvJkkFsWpm7Q=s2560"
+    },
+    {
+        name: 'air',
+        beats: ['spirit', 'water'],
+        imgLink: "https://lh3.googleusercontent.com/drive-viewer/AAOQEOSoYt6qfxugqKlNlc_lCUAMeSLJ53PGvZRHOS0QPBQtcwfn5mGoA8TaCOBbiwUB0e7rG_En6gXAi3LGNRkCqRjYWyxlow=s2560"
+    },
+    {
+        name: 'fire',
+        beats: ['air', 'spirit'],
+        imgLink: "https://lh3.googleusercontent.com/drive-viewer/AAOQEOTfnx1a8z6KczG9bVJAUzh1EyGwM9k7AO4G0pb0ssF2Q37gG30aEcwjBjlp6I5ZmLM1jMY-Mz5EsGecxjRF8LrL8E_UbA=s2560"
+    },
+    {
+        name: 'water',
+        beats: ['earth', 'fire'],
+        imgLink: "https://lh3.googleusercontent.com/drive-viewer/AAOQEOT37JbXKW4WoppiVg8ne1-gpWi5qvImIx1_WNI6YF8RW9SHbKTlMTxX9iS6DFGtSU0qGPFGnPZ7W-0UQvF_2mPWyd9BVA=s2560"
+    },
+    {
+        name: 'spirit',
+        beats: ['water', 'earth'],
+        imgLink: "https://lh3.googleusercontent.com/drive-viewer/AFGJ81p44Z9xQFFld30r608MIp0pdznnPVjpV3zTtRZYY5ZQkHNjeSz2M0oZ5X6dA8vSn5Z3OnvuU8Wc6gRWPChBkr_LpIBnkA=s2560"
+    },
+]
 
 //Global Variables
 var playClassicButton = document.querySelector('#classic-button');
@@ -66,14 +108,14 @@ function showClassicGame(){
     hide(homePage)
     show(classicGamePage)
     show(backHomeButton)
-    currentGame = 'classic'
+    game.mode = 'classic'
 };
 
 function showBenderGame(){
     hide(homePage)
     show(benderGamePage)
     show(backHomeButton)
-    currentGame = 'bender'
+    game.mode = 'bender'
 };
 
 function goBackToHome(){
@@ -92,10 +134,7 @@ function showResultsPage(){
 //functions to play game
 
 function selectUserFighter(e){
-    //e is target - write code to create userFighter based off of e and invoke takeTurn function
-    //bug with user fighter --- maybe undefined becasue the computer always wins
     var userFighter;
-
     if (e.target.classList.contains('rock')){
         userFighter = 'rock'
     } else if (e.target.classList.contains('paper')){
@@ -118,19 +157,19 @@ function selectUserFighter(e){
 
 function takeTurn(userFighter){
     var currentFighters = []
-    var computerFighter = randomFighter(currentGame)
+    var computerFighter = randomFighter(game.mode)
     currentFighters.push(userFighter)
     currentFighters.push(computerFighter)
     determineWinner(currentFighters)
 };
 
-function randomFighter(game){
+function randomFighter(gameMode){
     var classicOptions = ['rock', 'paper','scissors'];
     var benderOptions = ['water', 'earth', 'air', 'fire', 'spirit'];
-    if (game === 'classic') {
+    if (gameMode === 'classic') {
         var index = getRandomIndex(classicOptions)
         return classicOptions[index]
-    } else if (game === 'bender'){
+    } else if (gameMode === 'bender'){
         var index = getRandomIndex(benderOptions)
         return benderOptions[index]
     }
@@ -142,62 +181,37 @@ function getRandomIndex(array) {
 
 function determineWinner(fighters){
     var winner; 
-    if (fighters[0]===fighters[1]){
-        winner = 'none';
-    } else if (fighters[0]==='paper' && fighters[1]==='rock'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='scissors' && fighters[1]==='paper'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='rock' && fighters[1]==='scissors'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='water' && fighters[1]==='rock'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='water' && fighters[1]==='rock'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='earth' && fighters[1]==='fire'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='earth' && fighters[1]==='air'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='air' && fighters[1]==='water'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='air' && fighters[1]==='spirit'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='fire' && fighters[1]==='spirit'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='fire' && fighters[1]==='air'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='spirit' && fighters[1]==='earth'){
-        user.wins += 1
-        winner = 'user';
-    } else if (fighters[0]==='spirit' && fighters[1]==='water'){
-        user.wins += 1
-        winner = 'user';
-    } else {
-        computer.wins += 1
-        winner = 'computer'
-    } 
-    displayWins()
+    for (var i=0; i<fightersInfo.length; i++){
+        var winner;
+        if(fighters[0]===fighters[1]){
+            winner = 'none'
+        } else if(fighters[0]===fightersInfo[i].name && fightersInfo[i].beats.includes(fighters[1])){
+            winner = 'user'
+        } else if (fighters[0]===fightersInfo[i].name && !fightersInfo[i].beats.includes(fighters[1])){
+            winner = 'computer'
+        }
+    }
+    updateWins(winner)
     displayResults(fighters, winner)
     return winner
 };
 
+function updateWins(winner){
+    if(winner === 'user'){
+        game.players[0].wins += 1
+    } else if (winner === 'computer'){
+        game.players[1].wins += 1
+    }
+    displayWins()
+};
+
 function displayWins(){
-    userWins.innerText = `Wins: ${user.wins}`
-    computerWins.innerText = `Wins: ${computer.wins}`
+    userWins.innerText = `Wins: ${game.players[0].wins}`
+    computerWins.innerText = `Wins: ${game.players[1].wins}`
 };
 
 function displayResults(fighters, winner){
+    setTimeout(resetPage, 5000)
     hide(backHomeButton)
     if (winner === 'user'){
         winnerStatement.innerText = `You WIN! 🎉`
@@ -208,7 +222,6 @@ function displayResults(fighters, winner){
     }
     changeFighterImages(fighters)
     showResultsPage()
-    setTimeout(resetPage, 7000)
 };
 
 function changeFighterImages(fighters){
@@ -245,24 +258,15 @@ function chooseImage(fighter){
 function resetPage(){
     hide(resultsPage)
     show(backHomeButton)
-    if (currentGame === 'classic'){
+    if (game.mode === 'classic'){
         show(classicGamePage)
     } else {
         show(benderGamePage)
     }
 };
-
-/* connecting game to DOM/visual
-- need event listener on main section - listens for click on page and targets the specific image - iterate trhough array of stored image ids to find matching id - store each image as a variable in array as id - use just name (aka 'fire') as id 
-- This id will be the userFighter
-
-
-savedPalettesSection.addEventListener('click', function (e) {
-    if (e.target.classList.contains("delete-button")) {
-        confirmDelete(e)
-    }
-    displaySaved();
-});
-
-we invoke takeTurn function on click with userFighter being determined by the event target
+/* Things to Refactor:
+- store and access players and the currentGame through the game object --- game object becomes data model ✅
+- store fighters in array of objects - store their name, img, and who they beat in there ✅
+    - refactor long if else statments into for loops that look for that conditional
+- maybe instead of using fighters array over and over store each fighter in the data model and access it by name to help with readbility.
 */
